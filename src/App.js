d6c41418c7ce2ce9cb1913c7c6950b99d07f4893
soofiyasadaf <soofiyasadaf12345@gmail.com>
2021-12-23 22:19:04 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import './App.css';
 import Card from "./Card";
 import axios from 'axios';
 
-const data = {
-  results: [
-    {name: 'Walter White', nickname: 'Heisenberg', occupation:  
-    'High School Chemistry Teacher',  birthday: '09-07-1958', category: 'Breaking Bad', char_id: '1', src: 'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg' },
-    {name: 'Jesse Pinkman',   nickname: 'Cap  Cook',  occupation: 'Meth Dealer', birthday: '09-24-1984', category: 'Breaking Bad', char_id: '2', src: 'https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441'},
-    { name: 'Skyler White', nickname: 'sky', occupation:  'House wife, Book Keeper, Car Wash Manager, Taxi Dispatcher', birthday: '08-11-1970', category: 'Breaking Bad', char_id: '3',
-   src:'https://s-i.huffpost.com/gen/1317262/images/o-ANNA-GUNN-facebook.jpg' },
-   {name: 'Henry Schrader', nickname: 'Hank', occupation: 'DEA Agent', birthday: 'Unknown', category: 'Breaking Bad', char_id: '5', 
-    src: 'https://vignette.wikia.nocookie.net/breakingbad/images/b/b7/HankS5.jpg/revision/latest/scale-to-width-down/700?cb=20120620014136'}
+// const data = {
+//   results: [
+//     {name: 'Walter White', nickname: 'Heisenberg', occupation:  
+//     'High School Chemistry Teacher',  birthday: '09-07-1958', category: 'Breaking Bad', char_id: '1', src: 'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg' },
+//     {name: 'Jesse Pinkman',   nickname: 'Cap  Cook',  occupation: 'Meth Dealer', birthday: '09-24-1984', category: 'Breaking Bad', char_id: '2', src: 'https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441'},
+//     { name: 'Skyler White', nickname: 'sky', occupation:  'House wife, Book Keeper, Car Wash Manager, Taxi Dispatcher', birthday: '08-11-1970', category: 'Breaking Bad', char_id: '3',
+//    src:'https://s-i.huffpost.com/gen/1317262/images/o-ANNA-GUNN-facebook.jpg' },
+//    {name: 'Henry Schrader', nickname: 'Hank', occupation: 'DEA Agent', birthday: 'Unknown', category: 'Breaking Bad', char_id: '5', 
+//     src: 'https://vignette.wikia.nocookie.net/breakingbad/images/b/b7/HankS5.jpg/revision/latest/scale-to-width-down/700?cb=20120620014136'}
     
   
-  ]
-}
-console.log(data.results)
+//   ]
+// }
+// console.log(data.results)
 
 // export default App() 
 
@@ -39,14 +39,14 @@ function App() {
 
   const getAllCharacters = () => {
     axios.get("https://www.breakingbadapi.com/api/characters")
-    .then((res) => setCharacters(res))
+    .then((res) => setCharacters(res.data))
     .catch((err) => console.log(err));
   }
 
   console.log("characters", characters);
 
   const renderListUsingMap = () =>{
-    const mappedList = data.results.map((item,idx) => <Card  name={item.name} nickname={item.name} occupation={item.occupation} birthday={item.birthday} category={item.category} char_id={item.char_id} src={item.src}/> )
+    const mappedList =characters.map((item,idx) => <Card  name={item.name} nickname={item.name} occupation={item.occupation} birthday={item.birthday} category={item.category} char_id={item.char_id} img={item.img}/> )
    return mappedList
    }
   
--- src/Card.js
+++ src/Card.js
@@ -7,13 +7,13 @@
       return(
           <div className="card1">
               {/* <h1>Contact card of </h1> */}
-              <img src={props.src} alt="" />
-              <p> Name: {props.name}</p>
-              <p>Nickname: {props.nickname}</p>
-              <p>Occupation: {props.occupation}</p>
-              <p>Birthday: {props.birthday}</p>
-              <p>Category: {props.category}</p>
-              <p>Char_id: {props.char_id}</p>
+              <img className="img" src={props.img} alt="" />
+              <p  className="p1"> Name: {props.name}</p>
+              <p  className="p2">Nickname: {props.nickname}</p>
+              <p  className="p3">Occupation: {props.occupation}</p>
+              <p  className="p4">Birthday: {props.birthday}</p>
+              <p className="p5">Category: {props.category}</p>
+              <p  className="p6">Char_id: {props.char_id}</p>
 
           </div>
       );
